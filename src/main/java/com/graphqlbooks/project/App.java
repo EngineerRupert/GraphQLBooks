690ad7f523530fd09242433e5c5cdd595c0baff5
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2022-04-11 00:19:08 +0300

    Clear empty lines

--- src/main/java/com/graphqlbooks/project/App.java
+++ src/main/java/com/graphqlbooks/project/App.java
@@ -3,7 +3,6 @@ package com.graphqlbooks.project;
 import com.graphqlbooks.project.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class App {
