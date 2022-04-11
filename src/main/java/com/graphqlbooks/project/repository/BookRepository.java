3e72664003d90b9f9c9935b780964cf9c5cb1e69
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2022-04-11 21:22:40 +0300

    Added new BookRepository function

--- src/main/java/com/graphqlbooks/project/repository/BookRepository.java
+++ src/main/java/com/graphqlbooks/project/repository/BookRepository.java
@@ -2,8 +2,16 @@ package com.graphqlbooks.project.repository;
 
 import com.graphqlbooks.project.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    @Query("from Book b, Author a where a.name = :name")
+    List<Book> getBookByAuthor(@Param("name") String authorName);
+
 }
