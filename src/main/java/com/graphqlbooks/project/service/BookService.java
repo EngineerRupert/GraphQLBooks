4e9e3c5ec1d5acb933ed9ceaa90a816b4eee2dc9
EngineerRupert <77853142+EngineerRupert@users.noreply.github.com>
2022-04-12 23:33:52 +0300

    Clearing BookService

--- src/main/java/com/graphqlbooks/project/service/BookService.java
+++ src/main/java/com/graphqlbooks/project/service/BookService.java
@@ -1,7 +1,6 @@
 package com.graphqlbooks.project.service;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-import com.graphqlbooks.project.model.Author;
 import com.graphqlbooks.project.model.Book;
 import com.graphqlbooks.project.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
